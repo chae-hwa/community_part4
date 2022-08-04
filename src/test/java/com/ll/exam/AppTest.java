a665546e2a87cb410e9b9d11f3bd67856f1e2d77
chae-hwa <ihj0519@naver.com>
2022-08-05 02:10:24 +0900

    modify 테스트 추가 및 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,19 +1,70 @@
 package com.ll.exam;
 
-import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.article.dto.ArticleDto;
-import com.ll.exam.article.repository.ArticleRepository;
 import com.ll.exam.article.service.ArticleService;
-import com.ll.exam.home.controller.HomeController;
+import com.ll.exam.mymap.MyMap;
 import com.ll.exam.util.Ut;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppTest {
+   @BeforeAll
+   public void BeforeAll() {
+      MyMap myMap = Container.getObj(MyMap.class);
+
+      // 모든 DB 처리시에, 처리되는 SQL을 콘솔에 출력
+      myMap.setDevMode(true);
+   }
+
+   @BeforeEach
+   public void beforeEach() {
+      // 게시물 테이블을 깔끔하게 삭제한다.
+      // DELETE FROM article; // 보다 TRUNCATE article; 로 삭제하는게 더 깔끔하고 흔적이 남지 않는다.
+      truncateArticleTable();
+      // 게시물 3개를 만든다.
+      // 테스트에 필요한 샘플데이터를 만든다고 보면 된다.
+      makeArticleTestData();
+   }
+
+   private void makeArticleTestData() {
+      MyMap myMap = Container.getObj(MyMap.class);
+
+      IntStream.rangeClosed(1, 3).forEach(no -> {
+         boolean isBlind = false;
+         String title = "제목%d".formatted(no);
+         String body = "내용%d".formatted(no);
+
+         myMap.run("""
+                    INSERT INTO article
+                    SET createdDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+      });
+   }
+
+   private void truncateArticleTable() {
+      MyMap myMap = Container.getObj(MyMap.class);
+      // 테이블을 깔끔하게 지워준다.
+      myMap.run("TRUNCATE article");
+   }
    @Test
     public void 존재한다(){
        ArticleService articleService = Container.getObj(ArticleService.class);
@@ -65,6 +116,24 @@ public class AppTest {
       assertThat(articleDto.getCreatedDate()).isNotNull();
       assertThat(articleDto.getModifiedDate()).isNotNull();
       assertThat(articleDto.isBlind()).isEqualTo(false);
+   }
+
+   @Test
+   public void modify(){
+      ArticleService articleService = Container.getObj(ArticleService.class);
+
+      articleService.modify(1,"제목 new","내용 new",true);
+
+      ArticleDto articleDto = articleService.getArticleById(1);
+
+      assertThat(articleDto.getId()).isEqualTo(1);
+      assertThat(articleDto.getTitle()).isEqualTo("제목 new");
+      assertThat(articleDto.getBody()).isEqualTo("내용 new");
+      assertThat(articleDto.isBlind()).isNotNull();
+
+
+      long diffSeconds = ChronoUnit.SECONDS.between(articleDto.getModifiedDate(), LocalDateTime.now());
+      assertThat(diffSeconds).isLessThanOrEqualTo(1L);
    }
 }
 
